72f4d2b4801dc4051b6f8dbd8eecb36a2151589b
Alexito-17 <73041509+Alexito-17@users.noreply.github.com>
2022-06-16 10:24:35 -0500

    update something

--- src/compents/navBarOptions/NavBarOptions.js
+++ src/compents/navBarOptions/NavBarOptions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Tab } from 'bootstrap'
 import React from 'react'
 import Tabs from 'react-bootstrap/Tabs'
@@ -29,36 +28,4 @@ const NavBarOptions = () => {
     )
 }
 
-=======
-import { Tab } from 'bootstrap'
-import React from 'react'
-import Tabs from 'react-bootstrap/Tabs'
-import { CardCharacter } from '../cardCharacter/CardCharacter.js'
-import { TableWeapons } from '../tableWeapons/TableWeapons.js'
-
-import './NavBarOptions.css'
-
-const NavBarOptions = () => {
-
-    const backToTop = () =>{
-        document.body.scrollTop = 0
-        document.documentElement.scrollTop = 0
-    }
-
-
-    return (
-        <div className='container-fluid c-nav' >
-            <Tabs defaultActiveKey="Characters" id="Tab-c-w-a" onClick={backToTop}>
-                <Tab eventKey="Characters" title="Characters">
-                    <CardCharacter />
-                </Tab>
-                <Tab eventKey="Weapons" title="Weapons">
-                    <TableWeapons />
-                </Tab>
-            </Tabs>
-        </div>
-    )
-}
-
->>>>>>> 6c6f7e9508abc60f73cca721e22d0b5a67590ab9
 export default NavBarOptions
